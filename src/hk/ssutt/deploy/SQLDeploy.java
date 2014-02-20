e2fbc15405eb438ed6d3d993c8f868e36644867a
Vlad Slepukhin <slp.vld@gmail.com>
2014-02-20 13:01:36 +0400

    merged deployment class from ka2m/SQLAdapter

--- src/hk/ssutt/deploy/SQLDeploy.java
+++ src/hk/ssutt/deploy/SQLDeploy.java
@@ -1,6 +1,12 @@
 package hk.ssutt.deploy;
 
+import java.io.File;
 
 public class SQLDeploy {
-    //here we move all the deploy thing for sql
+    public SQLDeploy() {
+        long start = System.currentTimeMillis();
+        new DeployDB(new File("").getAbsolutePath());
+        long end = System.currentTimeMillis();
+        System.out.println("Database deployed in: " + (end - start) / 1_000 + " sec");
+    }
 }
---
+++ src/hk/ssutt/testing/TestingMain.java
@@ -0,0 +1,11 @@
+package hk.ssutt.testing;
+
+import hk.ssutt.deploy.SQLDeploy;
+/**
+ * Created by fau on 2/20/14.
+ */
+public class TestingMain {
+    public static void main(String[] args) {
+        SQLDeploy sqld = new SQLDeploy();
+    }
+}
